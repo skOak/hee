fc4567edba543b0cb169b391e60da619012ed8e8
白杨 <yangweichang@innotechx.com>
2020-06-10 22:14:28 +0800

    feat: 增加指定目录解析

--- cmd/commands/generate/generate.go
+++ cmd/commands/generate/generate.go
@@ -74,6 +74,7 @@ func init() {
 	CmdGenerate.Flag.Var(&generate.Fields, "fields", "List of table Fields.")
 	CmdGenerate.Flag.Var(&generate.DDL, "ddl", "Generate DDL Migration")
 	CmdGenerate.Flag.Var(&generate.Path, "path", "path of the generate destination")
+	CmdGenerate.Flag.Var(&generate.Include, "include", "path of the generate destination")
 	CmdGenerate.Flag.BoolVar(&generate.DownSwagger, "downdoc", false, "Enable auto-download of the swagger file if it does not exist.")
 	commands.AvailableCommands = append(commands.AvailableCommands, CmdGenerate)
 }
@@ -103,7 +104,7 @@ func GenerateCode(cmd *commands.Command, args []string) int {
 		if generate.Path.String() != "" {
 			dstPath, _ = filepath.Abs(generate.Path.String())
 		}
-		swaggergen.GenerateDocs(currpath, generate.DownSwagger, dstPath)
+		swaggergen.GenerateDocs(currpath, generate.DownSwagger, dstPath, generate.Include)
 	case "appcode":
 		appCode(cmd, args, currpath)
 	case "migration":
